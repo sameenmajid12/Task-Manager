e6724adc4718e7cb81c2c83e848fae9e9145ea76
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-12-01 14:48:28 -0500

    Setup app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./features/tasks/pages/task-list/task-list').then((m) => m.TaskList);
+    },
+  },
+  {
+    path:'tasks/:id',
+    loadComponent:()=>{
+      return import('./features/tasks/pages/task-detail/task-detail').then((m)=>m.TaskDetail);
+    }
+  },
+  {
+    path: 'categories',
+    loadComponent: () => {
+      return import('./features/categories/pages/category-list/category-list').then(
+        (m) => m.CategoryList
+      );
+    },
+  },
+];
