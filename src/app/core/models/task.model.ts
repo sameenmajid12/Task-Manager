7363155668e28b616551be5bfd7ca51d331c4894
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-12-02 01:09:52 -0500

    Add category to task model

--- src/app/core/models/task.model.ts
+++ src/app/core/models/task.model.ts
@@ -1,7 +1,10 @@
+import { Category } from "./category.model";
+
 export interface Task {
   id:number;
   title:string;
   description:string;
   completed:boolean;
   createdAt:string;
+  category: Category
 }
