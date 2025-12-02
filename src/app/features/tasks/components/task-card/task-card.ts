0a8cabd4e2c6d271c09f2d75b84380d3ce77d7c8
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-12-02 01:10:49 -0500

    Create task card component and implement into task list page

--- src/app/features/tasks/components/task-card/task-card.ts
+++ src/app/features/tasks/components/task-card/task-card.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, input, OnInit } from '@angular/core';
+import { Task } from '../../../../core/models/task.model';
 @Component({
   selector: 'app-task-card',
   imports: [],
   templateUrl: './task-card.html',
   styleUrl: './task-card.css',
 })
-export class TaskCard {
-
+export class TaskCard{
+  task = input.required<Task>();
 }
--- src/app/features/tasks/pages/task-list/task-list.ts
+++ src/app/features/tasks/pages/task-list/task-list.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TaskService } from '../../../../core/services/task.service';
+import { TaskCard } from '../../components/task-card/task-card';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [TaskCard],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
-export class TaskList {
-
+export class TaskList{
+  taskService = inject(TaskService)
+  taskList = this.taskService.tasks$();
 }
